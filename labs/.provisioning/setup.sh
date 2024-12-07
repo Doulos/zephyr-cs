aca12d1917b3550e4e405bb8c6d89935d6bbbe8d
ldomaigne <loic.domaigne@doulos.com>
2024-12-07 21:38:09 +0100

    sync

--- labs/.provisioning/setup.sh
+++ labs/.provisioning/setup.sh
@@ -5,6 +5,7 @@ west init -l labs
 west update --narrow -o=--depth=1
 west zephyr-export
 pip install -r deps/zephyr/scripts/requirements.txt
+pip install rich
 echo "alias ll='ls -lah'" >> $HOME/.bashrc
 west config build.dir-fmt /zephyr-training/build
 west completion bash > $HOME/west-completion.bash
@@ -18,15 +19,4 @@ HISTSIZE=5000
 HISTCONTROL=ignoredups:erasedups
 shopt -s histappend
 PROMPT_COMMAND="history -n; history -w; history -c; history -r; $PROMPT_COMMAND"
-if [[ "$TERM_PROGRAM" == "vscode" ]]; then
-    echo "==============================================================="
-    echo "!!!!!          TEST SYSTEM FOR IT INFRASTRUCTURE          !!!!!"
-    echo "==============================================================="
-    echo ""
-    echo "TEST System for Doulos Zephyr Essential training"
-    echo "If you see this banner, your IT infrastructure should be fine"
-    echo
-    echo "Current date: $(date)"
-    echo "==============================================================="
-fi
 EOF
