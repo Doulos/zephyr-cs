9dc90212bb397d993574290ba311ec05e34efc4a
ldomaigne <loic.domaigne@doulos.com>
2024-12-09 21:33:50 +0100

    sync

--- labs/.provisioning/setup.sh
+++ labs/.provisioning/setup.sh
@@ -9,7 +9,6 @@ pip install rich
 echo "alias ll='ls -lah'" >> $HOME/.bashrc
 west config build.dir-fmt /zephyr-training/build
 west completion bash > $HOME/west-completion.bash
-history -c
 cat >> $HOME/.bashrc << 'EOF'
 # enable completion for west command
 source $HOME/west-completion.bash
@@ -19,4 +18,6 @@ HISTSIZE=5000
 HISTCONTROL=ignoredups:erasedups
 shopt -s histappend
 PROMPT_COMMAND="history -n; history -w; history -c; history -r; $PROMPT_COMMAND"
+export PATH="$PATH:/opt/toolchains/zephyr-sdk-0.16.8/arm-zephyr-eabi/bin/"
 EOF
+history -c
