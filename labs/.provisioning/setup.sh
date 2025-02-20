b44bb65c599528db594bb21f46c4f6fc4b242b16
ldomaigne <loic.domaigne@doulos.com>
2025-02-20 11:10:42 +0100

    sync

--- labs/.provisioning/setup.sh
+++ labs/.provisioning/setup.sh
@@ -6,10 +6,16 @@ west update --narrow -o=--depth=1
 west zephyr-export
 pip install -r deps/zephyr/scripts/requirements.txt
 pip install rich
-echo "alias ll='ls -lah'" >> $HOME/.bashrc
 west config build.dir-fmt /zephyr-training/build
 west completion bash > $HOME/west-completion.bash
+
 cat >> $HOME/.bashrc << 'EOF'
+# aliases to make student's life easier
+alias ll='ls -lah'
+alias cdlab='cd /zephyr-training/labs'
+alias glcdsim='python /zephyr-training/labs/tools/glcdsim.py'
+alias arm-zephyr-eabi-addr2line='/opt/toolchains/zephyr-sdk-0.16.8/arm-zephyr-eabi/bin/arm-zephyr-eabi-addr2line'
+
 # enable completion for west command
 source $HOME/west-completion.bash
 
@@ -18,6 +24,5 @@ HISTSIZE=5000
 HISTCONTROL=ignoredups:erasedups
 shopt -s histappend
 PROMPT_COMMAND="history -n; history -w; history -c; history -r; $PROMPT_COMMAND"
-export PATH="$PATH:/opt/toolchains/zephyr-sdk-0.16.8/arm-zephyr-eabi/bin/"
 EOF
-history -c
+cat /dev/null > ~/.bash_history && history -c
